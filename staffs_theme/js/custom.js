fa86ffdb072fa27c4bff1019e883254fee24cef4
jevans <jake.evans@Staffordshire.gov.uk>
2026-07-31 14:57:05 +0100

    Add files via upload

---
+++ staffs_theme/js/custom.js
@@ -0,0 +1,7 @@
+(function (Drupal) {
+  Drupal.behaviors.staffordshireTheme = {
+    attach: function (context, settings) {
+      console.log('Staffordshire Theme Loaded');
+    }
+  };
+})(Drupal);
